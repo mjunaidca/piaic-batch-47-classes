ac2792decf785628ef2d4c3d52493d9467492d68
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-03-27 23:27:35 +0500

    Add SQLModel and psycopg dependencies, create Todo table and implement create_todo endpoint

--- todo-server/app/main.py
+++ todo-server/app/main.py
@@ -1,14 +1,44 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, Field, create_engine, Session
+from contextlib import asynccontextmanager
 
 from app import settings
 
-todo_server: FastAPI = FastAPI()
+# Step 1: Database Table SCHEMA
+class Todo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str
+    description: str
 
-@todo_server.get("/")
-def hello():
-    return {"Hello": "World"}
 
-@todo_server.get("/db")
-def db_var():
-    return {"DB": settings.DATABASE_URL}
+# Connection to the database
+conn_str: str = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
 
+engine = create_engine(conn_str)
+
+def create_db_tables():
+    print("create_db_tables")
+    SQLModel.metadata.create_all(engine)
+    print("done")
+
+@asynccontextmanager
+async def lifespan(todo_server: FastAPI):
+    print("Server Startup")
+    create_db_tables()
+    yield
+
+# Table Data Save, Get
+
+todo_server: FastAPI = FastAPI(lifespan=lifespan)
+
+
+@todo_server.post("/todo")
+def create_todo(try_content: Todo):
+        session = Session(engine)
+        session.add(try_content)
+        session.commit()
+        session.refresh(try_content)
+        session.close()
+        return try_content
